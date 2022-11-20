c9b5eb93b7cbb97ea02a55a27ed02ac0f859eafd
Litoz <ilikethebrioche@gmail.com>
2022-11-20 18:01:53 +0100

    Friends list dinamic

--- components/Friends.js
+++ components/Friends.js
@@ -1,37 +1,32 @@
 import React from 'react';
-import { Button, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { Button, StyleSheet, Text, ScrollView, View, Alert } from 'react-native';
 import { Card } from 'react-native-paper';
 
-export default function Friends({ navigation }) {
+const friends = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+export default function Friends() {
   return (
     <ScrollView>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 1</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 2</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 3</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 4</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 5</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 6</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 7</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 8</Text>
-      </Card>
-      <Card style={{margin: 15, padding: 25}}>
-        <Text>Amis 9</Text>
-      </Card>
+      {friends.map((friend) => {
+        return(
+          <Card style={{margin: 15, padding: 25}}>
+            <Text>{friend.title}</Text>
+          </Card>
+        )
+      })}
     </ScrollView>
   );  
 }
