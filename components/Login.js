bbbd3030bafe22ab307fa783a9b7df25ad024a66
Litoz <ilikethebrioche@gmail.com>
2022-11-20 12:25:54 +0100

    Windows Implementation

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Card } from 'react-native-paper';
-import Constants from 'expo-constants'
 import Main from './Main';
 
 export default function Login() {
